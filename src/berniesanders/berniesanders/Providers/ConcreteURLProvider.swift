3d163ce72e82332ac7ed305fdab5cf38383cbfa0
Mike Stallard <mikestallard@gmail.com>
2015-09-18 18:55:27 -0700

    Fetch up to 30 news items/issues
    [#103720926]

--- src/berniesanders/berniesanders/Providers/ConcreteURLProvider.swift
+++ src/berniesanders/berniesanders/Providers/ConcreteURLProvider.swift
@@ -2,11 +2,11 @@ import Foundation
 
 class ConcreteURLProvider : URLProvider {
     func issuesFeedURL() -> NSURL! {
-        return NSURL(string: "https://search.berniesanders.tech/articles_en/berniesanders_com/_search?q=article_type:Issues&sort=created_at:desc")
+        return NSURL(string: "https://search.berniesanders.tech/articles_en/berniesanders_com/_search?q=article_type:Issues&sort=created_at:desc&size=30")
     }
     
     func newsFeedURL() -> NSURL! {
-        return NSURL(string: "https://search.berniesanders.tech/articles_en/berniesanders_com/_search?q=!article_type%3A(ExternalLink%20OR%20Issues)&sort=created_at:desc")
+        return NSURL(string: "https://search.berniesanders.tech/articles_en/berniesanders_com/_search?q=!article_type%3A(ExternalLink%20OR%20Issues)&sort=created_at:desc&size=30")
     }
     
     func bernieCrowdURL() -> NSURL! {
