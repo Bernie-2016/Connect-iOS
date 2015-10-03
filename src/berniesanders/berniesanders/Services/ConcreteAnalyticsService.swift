37c96f4a8896f2793875b3e856f687fdb92795ea
Mike Stallard <mikestallard@gmail.com>
2015-10-03 11:24:52 -0700

    Log metrics to Fabric when in production
    [#102002416]

--- src/berniesanders/berniesanders/Services/ConcreteAnalyticsService.swift
+++ src/berniesanders/berniesanders/Services/ConcreteAnalyticsService.swift
@@ -1,23 +1,37 @@
 import Foundation
 
+#if RELEASE
+    import Crashlytics
+#endif
+
 class ConcreteAnalyticsService : AnalyticsService {
     func trackCustomEventWithName(name: String) {
-        
+        #if RELEASE
+            Answers.logCustomEventWithName(name, customAttributes: nil)
+        #endif
     }
     
     func trackContentViewWithName(name: String, type: AnalyticsServiceContentType, id: String) {
-
+        #if RELEASE
+            Answers.logContentViewWithName(name, contentType: type.description, contentId: id, customAttributes: nil)
+        #endif
     }
     
     func trackError(error: NSError, context: String) {
-
+        #if RELEASE
+            Answers.logCustomEventWithName("\(context): \(error.description)", customAttributes: nil)
+        #endif
     }
     
     func trackShareWithActivityType(activityType: String, contentName: String, contentType: AnalyticsServiceContentType, id: String) {
-        
+        #if RELEASE
+            Answers.logShareWithMethod(activityType, contentName: contentName, contentType: contentType.description, contentId: id, customAttributes: nil)
+        #endif
     }
     
     func trackSearchWithQuery(query: String, context: AnalyticsSearchContext) {
-
+        #if RELEASE
+            Answers.logSearchWithQuery(query, customAttributes: [ "context": context.description ])
+        #endif
     }
 }
