8ea06c0a556b24fa8570050be71edcbbb83ec930
Mike Stallard <mikestallard@gmail.com>
2016-03-02 19:06:07 -0800

    Begin to lay out news feed cells
    [#114435349]

--- src/Connect/Connect/News/UI/NewsArticleCollectionViewCell.swift
+++ src/Connect/Connect/News/UI/NewsArticleCollectionViewCell.swift
@@ -2,9 +2,23 @@ import UIKit
 
 class NewsArticleCollectionViewCell: UICollectionViewCell {
     let imageView = UIImageView.newAutoLayoutView()
-    let dateLabel = UILabel.newAutoLayoutView()
     let titleLabel = UILabel.newAutoLayoutView()
-    var imageVisible = false
+    let dateLabel = UILabel.newAutoLayoutView()
+
+    var titleToImageConstaint: NSLayoutConstraint!
+    var titleToTopConstraint: NSLayoutConstraint!
+
+    var imageVisible: Bool {
+        get {
+            return !self.imageView.hidden
+        }
+        set {
+            self.imageView.hidden = !newValue
+            self.titleToTopConstraint.priority = newValue ? 100 : 999
+            self.titleToImageConstaint.priority = newValue ? 999 : 100
+            self.layoutSubviews()
+        }
+    }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -13,26 +27,44 @@ class NewsArticleCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
 
+        backgroundColor = UIColor.greenColor()
+
+        imageView.contentMode = .ScaleAspectFill
+        imageView.clipsToBounds = true
+
+        titleLabel.backgroundColor = UIColor.magentaColor()
+        dateLabel.backgroundColor = UIColor.blueColor()
+
         contentView.addSubview(imageView)
-        contentView.addSubview(dateLabel)
         contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
+
+        let borderPadding: CGFloat = 10
+        let verticalPadding: CGFloat = 13
 
         imageView.autoPinEdgeToSuperviewEdge(.Top)
         imageView.autoPinEdgeToSuperviewEdge(.Right)
         imageView.autoPinEdgeToSuperviewEdge(.Left)
-        imageView.autoSetDimension(.Height, toSize: 50)
+
+        titleLabel.numberOfLines = 4
+
+        NSLayoutConstraint.autoSetPriority(999, forConstraints: { () -> Void in
+            self.titleToImageConstaint = self.titleLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: self.imageView, withOffset: verticalPadding)
+        })
+
+        NSLayoutConstraint.autoSetPriority(100, forConstraints: { () -> Void in
+            self.titleToTopConstraint = self.titleLabel.autoPinEdgeToSuperviewEdge(.Top, withInset: verticalPadding)
+        })
+
+        titleLabel.autoPinEdgeToSuperviewEdge(.Right, withInset: borderPadding)
+        titleLabel.autoPinEdgeToSuperviewEdge(.Left, withInset: borderPadding)
 
         dateLabel.numberOfLines = 1
-        dateLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: imageView)
-        dateLabel.autoPinEdgeToSuperviewEdge(.Right)
-        dateLabel.autoPinEdgeToSuperviewEdge(.Left)
+        dateLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: titleLabel)
+        dateLabel.autoPinEdgeToSuperviewEdge(.Right, withInset: borderPadding)
+        dateLabel.autoPinEdgeToSuperviewEdge(.Left, withInset: borderPadding)
+        dateLabel.autoPinEdgeToSuperviewEdge(.Bottom, withInset: verticalPadding)
 
-        titleLabel.numberOfLines = 0
-        titleLabel.autoPinEdge(.Top, toEdge: .Bottom, ofView: dateLabel)
-        titleLabel.autoPinEdgeToSuperviewEdge(.Right)
-        titleLabel.autoPinEdgeToSuperviewEdge(.Left)
-        titleLabel.autoPinEdgeToSuperviewEdge(.Bottom)
-
-        backgroundColor = UIColor.blueColor()
+//        backgroundColor = UIColor.blueColor()
     }
 }
