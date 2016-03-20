5f0fb2980097d436969b0404c31156fff17efa47
Mike Stallard <mikestallard@gmail.com>
2016-03-19 22:24:47 -0700

    Try and wait longer for embedded content to load
    [#114435521]

--- src/Connect/Connect/Actions/UI/ActionAlertLoadingMonitor.swift
+++ src/Connect/Connect/Actions/UI/ActionAlertLoadingMonitor.swift
@@ -6,8 +6,28 @@ protocol ActionAlertLoadingMonitor {
 
 class StockActionAlertLoadingMonitor: ActionAlertLoadingMonitor {
     func waitUntilWebViewsHaveLoaded(webViews: [UIWebView], completionHandler: () -> ()) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1000000000), dispatch_get_main_queue(), {
-          completionHandler()
-        })
+        checkForLoaded(webViews, completionHandler: completionHandler)
+    }
+
+    private func checkForLoaded(webViews: [UIWebView], completionHandler: () -> ()) {
+        var webViewsLoaded = 0
+
+        for webView in webViews {
+            let resultString = webView.stringByEvaluatingJavaScriptFromString("document.documentElement.getElementsByTagName('iframe').length > 0 || document.documentElement.getElementsByTagName('script').length == 0")
+
+            if resultString == "true" {
+                webViewsLoaded += 1
+            }
+        }
+
+        if webViewsLoaded == webViews.count {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 2000000000), dispatch_get_main_queue(), {
+                completionHandler()
+            })
+        } else {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 200000000), dispatch_get_main_queue(), {
+                self.checkForLoaded(webViews, completionHandler: completionHandler)
+            })
+        }
     }
 }
