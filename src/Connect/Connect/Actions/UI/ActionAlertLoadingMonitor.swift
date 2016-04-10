a9b74e404b09c8dfc79e961ac14701c10a3ee64d
Mike Stallard <mikestallard@gmail.com>
2016-04-09 17:09:28 -0700

    Reintroduce the monitor :(
    Turns out we need to wait for the JS to load so that the Twitter iframes
    load their padding appropriately (so we can then turn it off).

--- src/Connect/Connect/Actions/UI/ActionAlertLoadingMonitor.swift
+++ src/Connect/Connect/Actions/UI/ActionAlertLoadingMonitor.swift
@@ -12,7 +12,7 @@ class StockActionAlertLoadingMonitor: ActionAlertLoadingMonitor {
     private func checkForLoaded(webViews: [UIWebView], completionHandler: () -> ()) {
         var webViewsLoaded = 0
         for webView in webViews {
-            let resultString = webView.stringByEvaluatingJavaScriptFromString("var iframes = document.body.getElementsByTagName('iframe') ; var scripts = document.body.getElementsByTagName('script') ; var heights = [] ; for(var i = 0; i < iframes.length ; i++) { if(iframes[i].name == 'fb_xdm_frame_https') { continue; } heights.push(iframes[i].scrollHeight); } var maxIframeHeight = Math.max.apply(null, heights); (scripts.length > 0 && iframes.length > 0 && maxIframeHeight > 200) || scripts.length == 0;")
+            let resultString = webView.stringByEvaluatingJavaScriptFromString("for(var iframes=document.body.getElementsByTagName('iframe'),scripts=document.body.getElementsByTagName('script'),iframeConfigured=!1,i=0;i<iframes.length;i++){var iframe=iframes[i],style=iframe.getAttribute('style');(style&&style.match('visibility')||iframe.src.match(/youtube.com\\/embed/))&&(iframeConfigured=!0)}for(var heights=[],i=0;i<iframes.length;i++)'fb_xdm_frame_https'!=iframes[i].name&&heights.push(iframes[i].scrollHeight);var maxIframeHeight=Math.max.apply(null,heights);(iframeConfigured&&scripts.length>0&&iframes.length>0&&maxIframeHeight>200)||0==scripts.length;")
 
             if resultString == "true" {
                 webViewsLoaded += 1
@@ -20,11 +20,11 @@ class StockActionAlertLoadingMonitor: ActionAlertLoadingMonitor {
         }
 
         if webViewsLoaded == webViews.count {
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1000000000), dispatch_get_main_queue(), {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 500000000), dispatch_get_main_queue(), {
                 completionHandler()
             })
         } else {
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 100000000), dispatch_get_main_queue(), {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 50000000), dispatch_get_main_queue(), {
                 self.checkForLoaded(webViews, completionHandler: completionHandler)
             })
         }
