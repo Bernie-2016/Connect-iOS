cf11b45b3211569cd28e4bf4aaf4c153c25588ad
Mike Stallard <mikestallard@gmail.com>
2016-05-13 21:18:52 -0700

    Update to more official URLs

--- src/Connect/Connect/Registration/Domain/UpcomingVoterRegistrationUseCase.swift
+++ src/Connect/Connect/Registration/Domain/UpcomingVoterRegistrationUseCase.swift
@@ -14,9 +14,9 @@ class StockUpcomingVoterRegistrationUseCase: UpcomingVoterRegistrationUseCase {
             VoterRegistrationInfo(stateName: "California", url: NSURL(string: "http://registertovote.ca.gov/")!),
             VoterRegistrationInfo(stateName: "New Mexico", url: NSURL(string: "https://portal.sos.state.nm.us/OVR/%28S%28od4445h5uj2f5tyucvvhszdf%29%29/WebPages/InstructionsStep1.aspx")!),
             VoterRegistrationInfo(stateName: "New Jersey", url: NSURL(string: "http://www.state.nj.us/state/elections/voting-information.html")!),
-            VoterRegistrationInfo(stateName: "Montana", url: NSURL(string: "http://www.dmv.org/mt-montana/voter-registration.php")!),
+            VoterRegistrationInfo(stateName: "Montana", url: NSURL(string: "http://www.sos.mt.gov/elections/Vote/")!),
             VoterRegistrationInfo(stateName: "Virgin Islands", url: NSURL(string: "http://www.vivote.gov/content/register-vote")!),
-            VoterRegistrationInfo(stateName: "North Dakota", url: NSURL(string: "http://www.dmv.org/nd-north-dakota/voter-registration.php")!),
+            VoterRegistrationInfo(stateName: "North Dakota", url: NSURL(string: "https://vip.sos.nd.gov/PortalList.aspx")!),
             VoterRegistrationInfo(stateName: "South Dakota", url: NSURL(string: "https://sdsos.gov/elections-voting/voting/register-to-vote/default.aspx")!),
 
         ]
